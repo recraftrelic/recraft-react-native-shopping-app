f84f1d3c78123164675a2662f505591a0339159e
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-02-24 19:21:06 +0530

    add icon and Background image

--- pages/Login/Index.tsx
+++ pages/Login/Index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, ViewStyle, StyleSheet, TextStyle, TextInput } from 'react-native';
+import { View, ViewStyle, StyleSheet, TextStyle, TextInput, ImageBackground } from 'react-native';
 import { AppConstants } from '../../config/DefaultConfig';
 import ThemedText from '../../components/UI/ThemedText';
 import useConstants from '../../hooks/useConstants';
 import RoundButton from '../../components/Base/RoundButton';
-import FlatButton from '../../components/Base/FlatButton';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// @ts-ignore
+const ImagePath = require("../../Recraftsoppify_aap_bg_effect.png")
 
 interface Props {
 }
@@ -14,37 +17,47 @@ const Login: React.FunctionComponent<Props> = ({
   const constants: AppConstants = useConstants();
 
   return (
-    <View style={style.container}>
-      <View style={style.topContainer}>
-        <ThemedText styleKey="textColor" style={style.title}>{constants.labelLogin}</ThemedText>
+    <ImageBackground source={ImagePath} style={{ width: '100%', height: '100%' }} >
+      <View style={style.container}>
+        <View style={style.topContainer}>
+          <ThemedText styleKey="textColor" style={style.title}>{constants.labelLogin}</ThemedText>
+        </View>
+        <View style={style.childContainer}>
+          <ThemedText style={style.inputLabel} styleKey="textColor">{"User Name"}</ThemedText>
+        </View>
+        <View style={style.childContainer}>
+          <TextInput
+            style={style.inputContainer}
+            placeholder="User Name"
+          />
+        </View>
+        <View style={style.childContainer}>
+          <ThemedText style={style.inputLabel} styleKey="textColor">{"Password"}</ThemedText>
+        </View>
+        <View style={style.childContainer}>
+          <TextInput
+            style={style.inputContainer}
+            placeholder="Enter Password"
+            secureTextEntry={true}
+          />
+        </View>
+        <View style={style.childContainer}>
+          <ThemedText style={style.forgotPassword} styleKey="textColor">{"Forget your password?"}</ThemedText>
+        </View>
+        <RoundButton label="SUBMIT" onPress={() => { alert("clicked") }} />
+        <View style={style.childContainer}>
+          <ThemedText style={style.forgotPassword} styleKey="textColor">{"Or Login With"}</ThemedText>
+        </View>
+        <View style={style.childContainer}>
+          <View style={[style.iconContainer, { backgroundColor: '#e3384c' }]}>
+            <Icon name="google" size={30} color="#900" style={style.Icon} />
+          </View>
+          <View style={[style.iconContainer, { backgroundColor: '#39579a' }]}>
+            <Icon name="facebook" size={30} color="#900" style={[style.Icon]} />
+          </View>
+        </View>
       </View>
-      <View style={style.childContainer}>
-      <ThemedText style={style.inputLabel} styleKey="textColor">{"User Name"}</ThemedText>
-      </View>
-      <View style={style.childContainer}>
-        <TextInput
-          style={style.inputContainer}
-          placeholder="User Name"
-        />
-      </View>
-      <View style={style.childContainer}>
-      <ThemedText style={style.inputLabel} styleKey="textColor">{"Password"}</ThemedText>
-      </View>
-      <View style={style.childContainer}>
-        <TextInput
-          style={style.inputContainer}
-          placeholder="Enter Password"
-          secureTextEntry={true}
-        />
-      </View>
-      <View style={style.childContainer}>
-        <ThemedText style={style.forgotPassword} styleKey="textColor">{"Forget your password?"}</ThemedText>
-      </View>
-      <RoundButton label="SUBMIT" onPress={() => {alert("clicked")}}/>
-      <View style={style.childContainer}>
-        <ThemedText style={style.forgotPassword} styleKey="textColor">{"Or Login With"}</ThemedText>
-      </View>
-    </View>
+    </ImageBackground>
   )
 };
 
@@ -59,6 +72,10 @@ interface Style {
   inputLabel: TextStyle;
   forgotPassword: TextStyle;
   title: TextStyle;
+  Icon: TextStyle;
+  iconContainer: ViewStyle;
+  containerBg: ViewStyle;
+  containerImage: ViewStyle;
 }
 
 const style: Style = StyleSheet.create<Style>({
@@ -66,13 +83,14 @@ const style: Style = StyleSheet.create<Style>({
     paddingLeft: 35,
     paddingRight: 35,
     fontSize: 16,
+    backgroundColor: 'transparent'
   },
   topContainer: {
     flexDirection: 'row',
     justifyContent: "center",
     paddingLeft: 10,
     paddingRight: 10,
-    paddingTop: 180,
+    paddingTop: 150,
     marginBottom: 50,
   },
   bottomContainer: {
@@ -92,7 +110,7 @@ const style: Style = StyleSheet.create<Style>({
     flexDirection: 'row',
     justifyContent: "center",
   },
-  forgotPassword : {
+  forgotPassword: {
     marginTop: 30,
     marginBottom: 15,
     fontSize: 16,
@@ -110,5 +128,20 @@ const style: Style = StyleSheet.create<Style>({
     fontSize: 28,
     fontWeight: "600",
     color: "#54afee"
+  },
+  iconContainer: {
+    borderRadius: 6,
+    margin: 12,
+    minWidth: 50,
+    shadowOffset: { width: 0, height: 8 },
+    shadowColor: 'black',
+    shadowOpacity: 0.2,
+
+  },
+  Icon: {
+    fontSize: 25,
+    padding: 15,
+    color: '#fff',
+    justifyContent: "center",
   }
 });
